28a335b1c9d1ccd98068fff9f778e1bf5af86165
faulus <faulus001@gmail.com>
2019-01-25 12:35:15 +0100

    TCPping

---
+++ TCPping.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path $PSScriptRoot\output.txt -append
+
+
+$url = "https://download.elifulkerson.com//files/tcping/0.39/tcping.exe"
+
+$output = "$PSScriptRoot\tcping.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+cd $PSScriptRoot
+
+.\tcping.exe tcping --header -h -n 10 ndes-alstomgroup.msappproxy.net
+
+Stop-Transcript
+
+
+
+
+
